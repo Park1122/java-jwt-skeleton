45b034bf83be281e0c4055503ae07a0fda54e033
Park Jungwook <cco2416@gmail.com>
2024-04-20 13:25:40 +0900

    Spring Security 설정 참고사이트 주석 추가

--- src/main/java/com/ldcc/javajwtskeleton/global/config/security/SecurityConfig.java
+++ src/main/java/com/ldcc/javajwtskeleton/global/config/security/SecurityConfig.java
@@ -15,6 +15,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/*
+참고 사이트: https://suddiyo.tistory.com/entry/Spring-Spring-Security-JWT-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-2
+ */
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
